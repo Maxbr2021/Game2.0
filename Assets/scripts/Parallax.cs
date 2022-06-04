25877473b89673e22defd02c9b925045b595f1c1
Max Braueer <maxbraueer@Maxs-MBP-2.fritz.box>
2022-06-04 17:17:25 +0200

    parallax

--- Assets/scripts/Parallax.cs
+++ Assets/scripts/Parallax.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    private float len, start;
+    [SerializeField] GameObject cam;
+    [SerializeField] float parallax;
     // Start is called before the first frame update
     void Start()
     {
-        
+        start = transform.position.x;
+        len = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float temp = (cam.transform.position.x * (1 - parallax));
+        float dist = (cam.transform.position.x * parallax);
+        transform.position = new Vector3(start + dist, transform.position.y, transform.position.z);
+
+        if (temp > start + len)
+        {
+            start += len*2;
+        }
     }
 }
